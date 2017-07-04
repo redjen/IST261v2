0d6bf83158f6dcf37d9319f4f4eb56313aee0cc3
Jennifer Mullen <redjen@gmail.com>
2017-07-04 15:28:54 -0700

    Update ContactDAO and deprecated ContactDaoTextImpl to prepare for M07A01

--- src/dao/ContactDao.java
+++ src/dao/ContactDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import contacts.Contact;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The interface ContactDao defines the behavior of a class that reads and
@@ -15,4 +16,20 @@ public interface ContactDao {
     * @return the list of contacts
     */
    public ArrayList<Contact> getContacts();
+   
+   /**
+    * Returns a list of existing test contacts
+    * 
+    * Per assignment requirements, this method should be called whenever there
+    * are no persisted contacts
+    * 
+    * @return the list of contacts
+    */
+   public ArrayList<Contact> getTestContacts();
+   
+   /**
+    * Persists the list of saved contacts
+    * @param contacts 
+    */
+   public void saveContacts(List<Contact> contacts);
 }
--- src/dao/ContactDaoTextImpl.java
+++ src/dao/ContactDaoTextImpl.java
@@ -4,16 +4,20 @@ import contacts.Contact;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * The ContactDaoTextImpl is a very simple Dao for storing and retrieving contacts.
+ * The ContactDaoTextImpl is a very simple Dao for storing and retrieving
+ * contacts.
  *
- * As of M06, this is only used to get stored contact data for testing
+ * This was used only used to get stored contact data for testing. It's being
+ * retained for historical and possible future purposes.
  *
  */
+@Deprecated
 public class ContactDaoTextImpl implements ContactDao {
 
    private final static String DATA_FILE = "resources/defaultData/contacts.txt";
@@ -26,6 +30,12 @@ public class ContactDaoTextImpl implements ContactDao {
     */
    @Override
    public ArrayList<Contact> getContacts() {
+      // unused pre M07
+      return getTestContacts();
+   }
+
+   @Override
+   public ArrayList<Contact> getTestContacts() {
       ArrayList contacts = new ArrayList();
 
       try (Scanner scanner = new Scanner(new File(DATA_FILE))) {
@@ -42,9 +52,14 @@ public class ContactDaoTextImpl implements ContactDao {
          }
       } catch (FileNotFoundException ex) {
          Logger.getLogger(ContactDaoTextImpl.class.getName()).log(Level.SEVERE, null, ex);
-      } 
+      }
 
       return contacts;
+   }
+
+   @Override
+   public void saveContacts(List<Contact> contacts) {
+      // no-op
    }
 
 }
