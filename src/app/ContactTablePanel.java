1878f49e4db32ebad1c995b3d9b8c664b4f92917
redjen <redjen@gmail.com>
2017-07-26 14:32:03 -0700

    Added contact search button

--- src/app/ContactTablePanel.java
+++ src/app/ContactTablePanel.java
@@ -4,6 +4,7 @@ import data.ContactTableModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 /**
  * ContactTablePanel is the view for displaying a table of contacts. It is the
@@ -13,50 +14,68 @@ import javax.swing.JTable;
  */
 public class ContactTablePanel extends CardPanelBase {
 
-   private final static String ICON_PATH = "resources/icons/contact/";
-   private final static String ADD_ICON = ICON_PATH + "ic_person_add_18pt.png";
-   private final static String DELETE_ICON = ICON_PATH + "ic_delete_black_18dp.png";
-   private static final String PERSON_ICON = ICON_PATH + "ic_person_18pt.png";
+    private final static String ICON_PATH = "resources/icons/contact/";
+    private final static String ADD_ICON = ICON_PATH + "ic_person_add_18pt.png";
+    private final static String DELETE_ICON = ICON_PATH + "ic_delete_black_18dp.png";
+    private static final String PERSON_ICON = ICON_PATH + "ic_person_18pt.png";
 
-   private final JButton addContactButton;
-   private final JButton deleteContactButton;
-   private final JButton detailButton;
-   private final JTable contactTable;
+    private final JButton addContactButton;
+    private final JButton deleteContactButton;
+    private final JButton detailButton;
+    private final JTextField searchField;
+    private final JButton searchButton;
+    private final JTable contactTable;
 
-   public ContactTablePanel(ContactTableModel model) {
-      super();
+    public ContactTablePanel(ContactTableModel model) {
+        super();
 
-      addContactButton = new JButton("Add", new ImageIcon(ADD_ICON));
-      addContactButton.setToolTipText("new contact");
-      addButton(addContactButton);
+        addContactButton = new JButton("Add", new ImageIcon(ADD_ICON));
+        addContactButton.setToolTipText("new contact");
+        addToButtonPanel(addContactButton);
 
-      deleteContactButton = new JButton("Delete", new ImageIcon(DELETE_ICON));
-      deleteContactButton.setToolTipText("delete contact");
-      addButton(deleteContactButton);
+        deleteContactButton = new JButton("Delete", new ImageIcon(DELETE_ICON));
+        deleteContactButton.setToolTipText("delete contact");
+        addToButtonPanel(deleteContactButton);
 
-      detailButton = new JButton("Details", new ImageIcon(PERSON_ICON));
-      detailButton.setToolTipText("show details");
-      addButton(detailButton);
+        detailButton = new JButton("Details", new ImageIcon(PERSON_ICON));
+        detailButton.setToolTipText("show details");
+        addToButtonPanel(detailButton);
 
-      contactTable = new JTable(model);
-      setContent(contactTable);
-      revalidate();
-   }
+        searchField = new JTextField(20);
+        searchField.setToolTipText("search term");
+        addToButtonPanel(searchField);
 
-   public JButton getAddContactButton() {
-      return addContactButton;
-   }
+        searchButton = new JButton("Search");
+        searchButton.setToolTipText("search");
+        addToButtonPanel(searchButton);
 
-   public JButton getDeleteContactButton() {
-      return deleteContactButton;
-   }
+        contactTable = new JTable(model);
+        setContent(contactTable);
+        revalidate();
+    }
 
-   public JButton getDetailButton() {
-      return detailButton;
-   }
+    public JButton getAddContactButton() {
+        return addContactButton;
+    }
 
-   public JTable getContactTable() {
-      return contactTable;
-   }
+    public JButton getDeleteContactButton() {
+        return deleteContactButton;
+    }
+
+    public JButton getDetailButton() {
+        return detailButton;
+    }
+
+    public JTextField getSearchField() {
+        return searchField;
+    }
+
+    public JButton getSearchButton() {
+        return searchButton;
+    }
+
+    public JTable getContactTable() {
+        return contactTable;
+    }
 
 }
