1ea90f6801ddcd0e67984318f743301da2f6318d
redjen <redjen@gmail.com>
2017-07-26 13:35:19 -0700

    Use PrintWriter when printing instead of System.out

--- src/data/PersonYouMayKnowQueue.java
+++ src/data/PersonYouMayKnowQueue.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
@@ -10,114 +11,117 @@ import java.util.ListIterator;
  */
 public class PersonYouMayKnowQueue {
 
-   private final LinkedList<PersonYouMayKnow> people;
+    private final LinkedList<PersonYouMayKnow> people;
 
-   public PersonYouMayKnowQueue() {
-      this.people = new LinkedList<>();
-      createTestData();
-      printTestData();
-   }
+    public PersonYouMayKnowQueue() {
+        this.people = new LinkedList<>();
+        createTestData();
 
-   /**
-    * Adds the new item, maintaining sort order
-    *
-    * @param person the item to add
-    */
-   public void addItem(PersonYouMayKnow person) {
-      ListIterator<PersonYouMayKnow> iter = people.listIterator();
-      boolean added = false;
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            printTestData(out);
+        }
+    }
 
-      if (people.isEmpty()) {
-         people.add(person);
-      } else {
-         while (iter.hasNext()) {
-            PersonYouMayKnow nextPerson = iter.next();
-            if (person.compareTo(nextPerson) >= 0) {
-               people.add(iter.previousIndex(), person);
-               added = true;
-               break;
-            }
-         }
-         if (!added) {
+    /**
+     * Adds the new item, maintaining sort order
+     *
+     * @param person the item to add
+     */
+    public void addItem(PersonYouMayKnow person) {
+        ListIterator<PersonYouMayKnow> iter = people.listIterator();
+        boolean added = false;
+
+        if (people.isEmpty()) {
             people.add(person);
-         }
-      }
-
-   }
-
-   /**
-    * Returns the first item that contains the specified string in any field
-    *
-    * @param searchTerm the string to search for
-    * @return the first matching item or null if not found
-    */
-   public PersonYouMayKnow getItem(String searchTerm) {
-
-      for (PersonYouMayKnow person : people) {
-         if (person.containsTerm(searchTerm)) {
-            return person;
-         }
-      }
-
-      return null;
-   }
-
-   /**
-    * Removes all instances of the item in the queue
-    *
-    * @param person tje item to remove
-    */
-   public void removeItem(PersonYouMayKnow person) {
-      ListIterator<PersonYouMayKnow> iter = people.listIterator();
-
-      if (!people.isEmpty()) {
-
-         while (iter.hasNext()) {
-            Person nextPerson = iter.next();
-            if (person.equals(nextPerson)) {
-               iter.remove();
+        } else {
+            while (iter.hasNext()) {
+                PersonYouMayKnow nextPerson = iter.next();
+                if (person.compareTo(nextPerson) >= 0) {
+                    people.add(iter.previousIndex(), person);
+                    added = true;
+                    break;
+                }
             }
-         }
-      }
-   }
+            if (!added) {
+                people.add(person);
+            }
+        }
 
-   @Override
-   public String toString() {
-      StringBuilder sb = new StringBuilder();
-      String printFormat = "%s, %s, %s, %s, %s%n";
+    }
 
-      if (people.isEmpty()) {
-         sb.append("No people found.");
-      } else {
-         for (PersonYouMayKnow p : people) {
-            sb.append(String.format(printFormat, p.getFullName(), p.getPhoneNumber(),
-                    p.getEmail(), p.getTwitterId(), p.getFacebookId()));
-         }
-      }
+    /**
+     * Returns the first item that contains the specified string in any field
+     *
+     * @param searchTerm the string to search for
+     * @return the first matching item or null if not found
+     */
+    public PersonYouMayKnow getItem(String searchTerm) {
 
-      return sb.toString();
-   }
+        for (PersonYouMayKnow person : people) {
+            if (person.containsTerm(searchTerm)) {
+                return person;
+            }
+        }
 
-   /**
-    * Creates test data for assignment "M08-A01 Implementing a LinkedList"
-    */
-   private void createTestData() {
+        return null;
+    }
 
-      addItem(new PersonYouMayKnow("final", "test item", "", "", "", "", 0, 0));
-      addItem(new PersonYouMayKnow("second", "test item", "", "", "", "", 5, 0));
-      addItem(new PersonYouMayKnow("first",
-              "test item", "", "", "", "", 5, 2));
-      addItem(new PersonYouMayKnow("third", "test item", "", "", "", "", 0, 10));
-   }
+    /**
+     * Removes all instances of the item in the queue
+     *
+     * @param person tje item to remove
+     */
+    public void removeItem(PersonYouMayKnow person) {
+        ListIterator<PersonYouMayKnow> iter = people.listIterator();
 
-   /**
-    * Prints test data for assignment "M08-A01 Implementing a LinkedList"
-    */
-   private void printTestData() {
-      System.out.println("Test data:");
-      System.out.println(this.toString());
+        if (!people.isEmpty()) {
 
-      System.out.println();
-   }
+            while (iter.hasNext()) {
+                Person nextPerson = iter.next();
+                if (person.equals(nextPerson)) {
+                    iter.remove();
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String printFormat = "%s, %s, %s, %s, %s%n";
+
+        if (people.isEmpty()) {
+            sb.append("No people found.");
+        } else {
+            for (PersonYouMayKnow p : people) {
+                sb.append(String.format(printFormat, p.getFullName(), p.getPhoneNumber(),
+                        p.getEmail(), p.getTwitterId(), p.getFacebookId()));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Creates test data for assignment "M08-A01 Implementing a LinkedList"
+     */
+    private void createTestData() {
+
+        addItem(new PersonYouMayKnow("final", "test item", "", "", "", "", 0, 0));
+        addItem(new PersonYouMayKnow("second", "test item", "", "", "", "", 5, 0));
+        addItem(new PersonYouMayKnow("first",
+                "test item", "", "", "", "", 5, 2));
+        addItem(new PersonYouMayKnow("third", "test item", "", "", "", "", 0, 10));
+    }
+
+    /**
+     * Prints test data for assignment "M08-A01 Implementing a LinkedList"
+     */
+    private void printTestData(PrintWriter out) {
+        out.println("Test data:");
+        out.println(this.toString());
+        out.println();
+        out.flush();
+    }
 
 }
